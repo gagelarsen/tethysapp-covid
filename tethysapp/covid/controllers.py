805f95a14ded725b17cefa7636dc26de659a5c03
Gage Larsen <gagelarsen53@gmail.com>
2020-02-28 08:25:40 +0300

    get plot showing

--- tethysapp/covid/controllers.py
+++ tethysapp/covid/controllers.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from tethys_sdk.gizmos import PlotlyView
 from tethys_sdk.permissions import login_required
 
+from datetime import datetime
+import plotly.graph_objs as go
 
 @login_required()
 def home(request):
@@ -37,53 +39,19 @@ def home(request):
         (178, "Japan")
     ]
 
-    line_plot_view = PlotlyView(
-        height='500px',
-        width='500px',
-        template='plotly_dark',
-        engine='highcharts',
-        title='Plot Title',
-        subtitle='Plot Subtitle',
-        spline=True,
-        x_axis_title='Date',
-        x_axis_units='days',
-        y_axis_title='Count',
-        series=[
-            {
-                'name': 'Mainland China',
-                'color': '#FFA500',
-                'data': [
-                    [0, 5], [10, 10],
-                    [20, 15], [30, 30],
-                    [40, 50],
-                    [50, 70], [60, 100],
-                    [70, 130], [80, 150]
-                ]
-            },
-            {
-                'name': 'Other',
-                'color': '#FFFF00',
-                'data': [
-                    [0, 0], [10, 2],
-                    [20, 4], [30, 6],
-                    [40, 10],
-                    [50, 15], [60, 20],
-                    [70, 24], [80, 26]
-                ]
-            },
-            {
-                'name': 'Recovered',
-                'color': '#008000',
-                'data': [
-                    [0, 0], [10, .25],
-                    [20, 1], [30, 2],
-                    [40, 3],
-                    [50, 5], [60, 7],
-                    [70, 10], [80, 14]
-                ]
-            }
-        ]
-    )
+    x_ = []
+
+    x_ = [datetime(year=2013, month=10, day=x) for x in range(1, 21)]
+
+    mainland = [6 * x for x in range(0, 20)]
+    recover = [3 * x for x in range(0, 20)]
+    other = [2 * x for x in range(0, 20)]
+
+    my_plotly_view = PlotlyView([
+        go.Scatter(x=x_, y=mainland),
+        go.Scatter(x=x_, y=recover),
+        go.Scatter(x=x_, y=other),
+    ])
 
     context = {
         'total_confirmed': total_confirmed,
@@ -93,7 +61,7 @@ def home(request):
         'conf_by_region': conf_by_region,
         'death_by_region': death_by_region,
         'recov_by_region': recov_by_region,
-        'plot_view': line_plot_view,
+        'plot_view': my_plotly_view,
     }
 
     return render(request, 'covid/home.html', context)
